95827cd50af9d42e388a6b38836d05a876adcda7
cheezgi <zackh@firemail.cc>
2016-11-23 03:06:01 +0000

    Initial commit

---
+++ main.lua
@@ -0,0 +1,20 @@
+-- lbar: simple bar that does stuff
+
+function love.load()
+end
+
+function love.update()
+end
+
+function love.draw()
+end
+
+function love.keypressed(key)
+end
+
+function love.mousepressed(x, y)
+end
+
+function love.mousereleased(x, y)
+end
+
